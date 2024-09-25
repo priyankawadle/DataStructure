b2935c062d08ba22b52a6a37a2e2c5752aece336
priyanka <wadle98priyanka@gmail.com>
2024-09-25 07:46:08 +0530

    bubble sorting corrcted

--- Array/bubblesorting.js
+++ Array/bubblesorting.js
@@ -1,12 +1,13 @@
 function bubblesortingArray(arr) {
     for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr.length; j++) {
-            if (arr[i] < arr[j]) {
-                const temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
+        for (let j = 0; j < arr.length-i-1; j++) { // (length - i - 1) because the last elements are already sorted
+            if (arr[j] < arr[j+1]) { // Compare adjacent elements
+                const temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
             }
         }
+        console.log(arr)
     }
     return arr;
 }
